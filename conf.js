3cc07fbd34a2459eee72284ef70fcff833280537
Velmurugan1401 <velvel1401@gmail.com>
2021-12-14 20:02:46 +0530

    user add api and authendication api

---
+++ conf.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORTNUMBER:2022,
+    DB:`mongodb+srv://velvel:lo0ewlp8yp9TYp9x@cluster0.obva9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    KEY:"TICKET_BOOKING"
+}
